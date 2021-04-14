39d9c2d936c7db731ce7571a5534bbdef18a835d
pnuzunov <pnuzunov@gmail.com>
2021-04-14 11:43:56 +0300

    Initial commit

---
+++ design-patterns-task-1/src/main/java/design_patterns_default_group/design_patterns_task_1/App.java
@@ -0,0 +1,13 @@
+package design_patterns_default_group.design_patterns_task_1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
